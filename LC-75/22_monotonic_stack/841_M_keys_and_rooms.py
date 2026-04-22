b0c0c52f6fd938ce670181f77d6fa76632726526
JohnCastles <johnjcastles@gmail.com>
2026-04-22 14:22:34 +0200

    841.M:keys and rooms

---
+++ LC-75/22_monotonic_stack/841_M_keys_and_rooms.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        """
+        Idea:
+        Stack: To keep track of rooms you can and still need to visit 
+        (dynamically adjusts depending on newly explored rooms and keys within it)
+        
+        Set: To keep track of already visited rooms (avoids endless loop)
+        """
+
+        stk = [0] # since room 0 is unlocked
+        visit = set()
+
+        while stk:
+            room_id = stk.pop()
+            
+            visit.add(room_id)
+            # add keys in this room to the stack only if they havent been visited yet
+            for key in rooms[room_id]:
+                if key not in visit:
+                    stk.append(key)
+        
+        return len(visit) == len(rooms)
+            
+        
