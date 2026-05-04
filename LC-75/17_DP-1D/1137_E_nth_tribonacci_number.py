6ba5e0b4be80c2e29aab1c5a9278a359831e25b3
JohnCastles <johnjcastles@gmail.com>
2026-05-04 13:01:33 +0200

    1137.E: nth tribonacci number

---
+++ LC-75/17_DP-1D/1137_E_nth_tribonacci_number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def __init__(self):
+        self.cache = {
+            0: 0,
+            1: 1,
+            2: 1
+        }
+    def tribonacci(self, n: int) -> int:
+        if n in self.cache:
+            return self.cache[n]
+        else:
+            sol = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+            self.cache[n] = sol
+            return sol
+    
+            
