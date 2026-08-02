4d638904a2b63abb2fb991901d6401cd2296f34c
JohnChristianCastillo <37139666+JohnChristianCastillo@users.noreply.github.com>
2026-08-02 21:06:09 +0200

    [1] Two Sum (Easy) - 8 ms, 13.6 MB

--- 0001_E_two-sum_02_aug_2026.py
+++ 0001_E_two-sum_02_aug_2026.py
@@ -1,12 +1,24 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mp = {}
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        """
+        convert nums s.t. it also holds the index
+        """
+        for i, v in enumerate(nums):
+            nums[i] = [v, i]
+        nums.sort()
+        l, r = 0, len(nums)-1
 
-        for i,v in enumerate(nums):
-            diff = target-v
-            if diff in mp:
-                return [mp[diff], i]
+        while l < r:
+            _sum = nums[l][0] + nums[r][0]
+            if _sum < target:
+                l += 1
+            elif _sum > target:
+                r -= 1
             else:
-                mp[v] = i
-        
-        return 
+                return [nums[l][1], nums[r][1]]
+        return []
