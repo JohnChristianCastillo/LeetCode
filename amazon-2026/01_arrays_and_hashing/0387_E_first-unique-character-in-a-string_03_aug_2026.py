efbcda059e7500c9337e6085fa0d11a0c41e4045
JohnChristianCastillo <37139666+JohnChristianCastillo@users.noreply.github.com>
2026-08-03 14:35:27 +0200

    [387] First Unique Character in a String (Easy) - 46 ms, 19.9 MB

---
+++ amazon-2026/01_arrays_and_hashing/0387_E_first-unique-character-in-a-string_03_aug_2026.py
@@ -0,0 +1,7 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        ctr = Counter(s)
+        for i, c in enumerate(s):
+            if ctr[c] == 1:
+                return i
+        return -1
