fff14fcd5684cef56c33109d7c38bbcdf7c76dad
JohnChristianCastillo <37139666+JohnChristianCastillo@users.noreply.github.com>
2026-08-04 13:44:42 +0200

    [15] 3Sum (Medium) - 613 ms, 22.3 MB

---
+++ amazon-2026/02_two-pointers/0015_M_3sum_04_aug_2026.py
@@ -0,0 +1,36 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        """
+        idea:
+        1. it is easy because we dont need to return the indices
+            -> sort the list
+        2. loop over each element until 3 elements left so until len(nums)-3
+            perform two sum II (two pointer)
+        """
+        nums = sorted(nums)
+        
+        sols = []
+        for i, v in enumerate(nums):
+            # !!!to avoid duplicate triplets, avoid duplicate anchors
+            if i > 0 and v == nums[i-1]:
+                continue
+
+            l, r = i+1, len(nums)-1
+            while l < r:
+                curr = v + nums[l] + nums[r]
+                if curr == 0:
+                    sols.append([v, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+
+                    # !!!make sure new indexes are not duplicates
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
+
+                elif curr < 0:
+                    l += 1
+                else:
+                    r -= 1
+        return sols
